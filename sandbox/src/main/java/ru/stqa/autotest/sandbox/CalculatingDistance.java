2d26f84ae590ce41b37944273f3c0ae120dcb07b
LikinaElizaveta <lisi4ka-90@inbox.ru>
2021-09-26 23:13:50 +0300

    Задание 2.2 Используем метод вместо функции

--- sandbox/src/main/java/ru/stqa/autotest/sandbox/CalculatingDistance.java
+++ sandbox/src/main/java/ru/stqa/autotest/sandbox/CalculatingDistance.java
@@ -6,11 +6,7 @@ public class CalculatingDistance {
 
     Point p1 = new Point(2,3);
     Point p2 = new Point(7,5);
-    System.out.println("Расстояние между точками (" + p1.x +";" + p1.y + ") и (" + p2.x +";" + p2.y + ") = " + distance(p1,p2));
-  }
-
-  public static double distance(Point p1, Point p2) {
-    return  Math.sqrt((p2.x- p1.x) * (p2.x-p1.x) + (p2.y - p1.y) * (p2.y - p1.y));
+    System.out.println("Расстояние между точками = "+ p2.distance(p1));
   }
 
 }
--- sandbox/src/main/java/ru/stqa/autotest/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/autotest/sandbox/Point.java
@@ -1,13 +1,16 @@
 package ru.stqa.autotest.sandbox;
 
 public class Point {
+  private final double x;
+  private final double y;
 
-  public double x;
-  public double y;
+  public Point(double x1, double y1) {
+    this.x = x1;
+    this.y = y1;
+  }
 
-  public Point (double x, double y) {
-    this.x = x;
-    this.y = y;
+  public double distance(Point p2) {
+    return Math.sqrt((p2.x- this.x) * (p2.x-this.x) + (p2.y - this.y) * (p2.y - this.y));
   }
 
 
