c9c50f30a06b30e89ccfd91a6811b50d88657b3e
LikinaElizaveta <lisi4ka-90@inbox.ru>
2021-09-26 23:10:17 +0300

    Задание 2.1 Создана функция для расчета расстояния между двумя точками. Создан запускаемый класс.

---
+++ sandbox/src/main/java/ru/stqa/autotest/sandbox/CalculatingDistance.java
@@ -0,0 +1,20 @@
+package ru.stqa.autotest.sandbox;
+
+public class CalculatingDistance {
+
+  public static void main(String[] args) {
+
+    Point p1 = new Point(2,3);
+    Point p2 = new Point(7,5);
+    System.out.println("Расстояние между точками (" + p1.x +";" + p1.y + ") и (" + p2.x +";" + p2.y + ") = " + distance(p1,p2));
+  }
+
+  public static double distance(Point p1, Point p2) {
+    return  Math.sqrt((p2.x- p1.x) * (p2.x-p1.x) + (p2.y - p1.y) * (p2.y - p1.y));
+  }
+
+}
+
+
+
+
---
+++ sandbox/src/main/java/ru/stqa/autotest/sandbox/Point.java
@@ -0,0 +1,14 @@
+package ru.stqa.autotest.sandbox;
+
+public class Point {
+
+  public double x;
+  public double y;
+
+  public Point (double x, double y) {
+    this.x = x;
+    this.y = y;
+  }
+
+
+}
