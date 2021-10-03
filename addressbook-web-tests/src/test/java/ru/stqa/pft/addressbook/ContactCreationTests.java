78a65b269d784cad9f2296f62a59f296ecf94248
LikinaElizaveta <lisi4ka-90@inbox.ru>
2021-10-03 11:36:08 +0300

    Доработка теста на создание контакта (инициализация вынесена в отдельный метод)

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactCreationTests.java
@@ -11,17 +11,17 @@ public class ContactCreationTests {
   @BeforeMethod(alwaysRun = true)
   public void setUp() throws Exception {
     wd = new FirefoxDriver();
-    wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-  }
-
-  @Test
-  public void testContactCreation() throws Exception {
+    wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
     wd.get("https://localhost/addressbook/");
     wd.findElement(By.name("user")).clear();
     wd.findElement(By.name("user")).sendKeys("admin");
     wd.findElement(By.name("pass")).clear();
     wd.findElement(By.name("pass")).sendKeys("secret");
     wd.findElement(By.id("LoginForm")).submit();
+  }
+
+  @Test
+  public void testContactCreation() throws Exception {
     wd.findElement(By.linkText("add new")).click();
     wd.findElement(By.name("firstname")).click();
     wd.findElement(By.name("firstname")).clear();
