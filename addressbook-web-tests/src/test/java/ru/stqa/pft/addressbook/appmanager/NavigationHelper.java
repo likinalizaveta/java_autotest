d2f8f258ef81e4336b5d54e0630fb6b2eaabf035
LikinaElizaveta <lisi4ka-90@inbox.ru>
2021-10-11 23:52:48 +0300

    Доработка HelperBase, добавление проверки в NavigationHelper

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -10,6 +10,9 @@ public class NavigationHelper extends HelperBase {
   }
 
   public void returnToHomePage() {
+    if (isElementPresent(By.id("maintable"))) {
+      return;
+    }
     click(By.linkText("home"));
   }
 }
