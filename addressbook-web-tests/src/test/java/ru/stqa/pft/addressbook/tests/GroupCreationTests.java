dbeb119ad620f3437227c0d41abb9ae791fc2d29
LikinaElizaveta <lisi4ka-90@inbox.ru>
2021-10-23 18:45:11 +0300

    Доработка проверки в тесте GroupCreationTests (использование уникальных идентификаторов объектов)

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -6,6 +6,7 @@ import ru.stqa.pft.addressbook.model.GroupData;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.Set;
 
 
 public class GroupCreationTests extends TestBase {
@@ -13,20 +14,17 @@ public class GroupCreationTests extends TestBase {
   @Test
   public void testGroupCreation() throws Exception {
     app.group().gotoGroupPage();
-    List<GroupData> before = app.group().list();
+    Set<GroupData> before = app.group().all();
     app.group().initGroupCreation();
     GroupData group = new GroupData().withName("test2");
     app.group().fillGroupForm(group);
     app.group().submitGroupCreation();
     app.group().returnToGroupPage();
-    List<GroupData> after = app.group().list();
+    Set<GroupData> after = app.group().all();
     Assert.assertEquals(after.size(), before.size() + 1);
 
-    group.withtId(after.stream().max((Comparator<GroupData>) (o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
+    group.withtId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt());
     before.add(group);
-    Comparator<? super GroupData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
-    before.sort(byId);
-    after.sort(byId);
     Assert.assertEquals(before, after);
   }
 
