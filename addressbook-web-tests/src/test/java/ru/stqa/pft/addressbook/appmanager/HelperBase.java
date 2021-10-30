25d4c65fc82848ed6ba0857c41b4fd478f295877
LikinaElizaveta <lisi4ka-90@inbox.ru>
2021-10-30 12:35:22 +0300

    Доработан тест на создание контакта, прикрепляем картинку к контакту

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/HelperBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/HelperBase.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.WebDriver;
 
 import org.openqa.selenium.NoSuchElementException;
 
+import java.io.File;
+
 public class HelperBase {
   protected WebDriver wd;
 
@@ -23,6 +25,12 @@ public class HelperBase {
      wd.findElement(locator).clear();
      wd.findElement(locator).sendKeys(text);
    }
+  }
+
+  protected void attach(By locator, File file) {
+    if (file != null) {
+      wd.findElement(locator).sendKeys(file.getAbsolutePath());
+    }
   }
 
   public boolean isAlertPresent() {
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -4,6 +4,8 @@ import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
 
+import java.io.File;
+
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
@@ -14,9 +16,10 @@ public class ContactCreationTests extends TestBase {
     app.goTo().homePage();
     Contacts before = app.contact().all();
     app.contact().gotoAddNewContactPage();
+    File photo = new File("src/test/resources/image.png");
     ContactData contact = new ContactData().withFirstname("Jeremy").withLastname("Martinson")
             .withAddress("455 Larkspur Dr.\nCalifornia Springs, CA 92926\nUSA").withPhoneHome("11111")
-            .withMobilePhone("22222").withWorkPhone("33333").withEmail("jmartinson@yahoo.com").withGroup("test1");
+            .withMobilePhone("22222").withWorkPhone("33333").withEmail("jmartinson@yahoo.com").withGroup("test1").withPhoto(photo);
     app.contact().fillNewContactForm(contact, true);
     app.contact().submitNewContactCreation();
     app.goTo().homePage();
@@ -26,12 +29,13 @@ public class ContactCreationTests extends TestBase {
             before.withAdded(contact.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
   }
 
-  @Test
+  @Test(enabled = false)
   public void testBedContactCreation() throws Exception {
     app.goTo().homePage();
     Contacts before = app.contact().all();
     app.contact().gotoAddNewContactPage();
-    ContactData contact = new ContactData().withFirstname("Jeremy'").withLastname("Martinson").withAddress("455 Larkspur Dr.\nCalifornia Springs, CA 92926\nUSA").withPhoneHome("11111").withMobilePhone("22222").withWorkPhone("33333").withEmail("jmartinson@yahoo.com").withGroup("test1");
+    ContactData contact = new ContactData().withFirstname("Jeremy'").withLastname("Martinson").withAddress("455 Larkspur Dr.\nCalifornia Springs, CA 92926\nUSA")
+            .withPhoneHome("11111").withMobilePhone("22222").withWorkPhone("33333").withEmail("jmartinson@yahoo.com").withGroup("test1");
     app.contact().fillNewContactForm(contact, true);
     app.contact().submitNewContactCreation();
     app.goTo().homePage();
@@ -39,5 +43,18 @@ public class ContactCreationTests extends TestBase {
     Contacts after = app.contact().all();
     assertThat(after, equalTo(before));
   }
+
+  @Test(enabled = false)
+  public void testCurrentDir() {
+    File currentDir = new File(".");
+    System.out.println(currentDir.getAbsolutePath());
+    File photo = new File("src/test/resources/image.png");
+    System.out.println(photo.getAbsolutePath());
+    System.out.println(photo.exists());
+  }
+
+
+
+
 
 }
